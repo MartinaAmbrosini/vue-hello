0aa0d0417f074985e483b05b4dd526639a20a497
Martina Ambrosini <martinaambrosini17@gmail.com>
2023-12-07 16:23:41 +0100

    feat: added hello world with Vue

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 // Descrizione:
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      message: "Hello World"
+    };
+  },
+}).mount("#app");
